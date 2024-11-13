59e6481360f03e46f8cb1023aba8e775dbd60f2e
Spax <83354374+SpiritAxolotl@users.noreply.github.com>
2024-11-12 22:37:48 -0700

    wait crap

--- data/config/user-classes.conf.php
+++ data/config/user-classes.conf.php
@@ -2,10 +2,6 @@
 
 namespace Shimmie2;
 
-new UserClass("user", "base", [
-	Permissions::CREATE_COMMENT => False
-]);
-
 new UserClass("hellbanned", "base", [
 	Permissions::CREATE_COMMENT => False
 ]);
