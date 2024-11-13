7c596653ad7efa7f3e837ea7958af3f6b7e08ab0
Spax <83354374+SpiritAxolotl@users.noreply.github.com>
2024-11-12 22:36:46 -0700

    override userclasses for no commenting

--- data/config/user-classes.conf.php
+++ data/config/user-classes.conf.php
@@ -2,4 +2,12 @@
 
 namespace Shimmie2;
 
+new UserClass("anonymous", "base", [
+	Permissions::CREATE_COMMENT => False
+]);
+
+new UserClass("hellbanned", "base", [
+	Permissions::CREATE_COMMENT => False
+]);
+
 new UserClass("trusted", "user");
