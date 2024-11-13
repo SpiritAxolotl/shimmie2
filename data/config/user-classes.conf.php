a431cd418ea95b9d9a8db89ff9d8b7ec42aabd50
Spax <83354374+SpiritAxolotl@users.noreply.github.com>
2024-11-12 22:44:11 -0700

    more fleshed out overwrite perms

--- data/config/user-classes.conf.php
+++ data/config/user-classes.conf.php
@@ -2,8 +2,37 @@
 
 namespace Shimmie2;
 
+new UserClass("user", "base", [
+	Permissions::CHANGE_USER_SETTING => True,
+	Permissions::BIG_SEARCH => True,
+	Permissions::CREATE_COMMENT => False,
+	Permissions::CREATE_IMAGE => False,
+	Permissions::EDIT_IMAGE_TAG => False,
+	Permissions::EDIT_IMAGE_SOURCE => False,
+	Permissions::EDIT_IMAGE_TITLE => False,
+	Permissions::EDIT_IMAGE_RELATIONSHIPS => False,
+	Permissions::EDIT_IMAGE_ARTIST => False,
+	Permissions::CREATE_IMAGE_REPORT => False,
+	Permissions::SEND_PM => False,
+	Permissions::READ_PM => False,
+	Permissions::CREATE_VOTE => False,
+	Permissions::EDIT_IMAGE_RATING => True,
+	Permissions::PERFORM_BULK_ACTIONS => True,
+	Permissions::EDIT_FAVOURITES => True,
+	Permissions::FORUM_CREATE => False,
+	Permissions::NOTES_CREATE => False,
+	Permissions::NOTES_EDIT => False,
+	Permissions::NOTES_REQUEST => True,
+	Permissions::POOLS_CREATE => False,
+	Permissions::POOLS_UPDATE => False,
+	Permissions::SET_PRIVATE_IMAGE => False,
+	Permissions::BULK_DOWNLOAD => False
+]);
+
 new UserClass("hellbanned", "base", [
 	Permissions::CREATE_COMMENT => False
 ]);
 
-new UserClass("trusted", "user");
+new UserClass("trusted", "user", [
+	Permissions::BULK_DOWNLOAD => True
+]);
